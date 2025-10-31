aad32cab01ac7fd02bda28f2ca4d5ed66793ddf0
Aditya <adityapat@cybage.com>
2025-10-31 17:22:54 +0530

    Added new components and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import { ControlFlow } from './components/control-flow/control-flow';
+import { Attribute } from './components/attribute/attribute';
+import { DataBinding } from './components/data-binding/data-binding';
+import { Signal } from './components/signal/signal';
+import { User } from './components/user/user';
+import { Notfound } from './components/notfound/notfound';
+import { Photos } from './components/photos/photos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'', component:User, pathMatch:'full'
+    },
+    {
+        path:'controlflow', component:ControlFlow
+    },
+    {
+        path:'attribute', component:Attribute
+    },
+    {
+        path:'data-binding', component:DataBinding
+    },
+    {
+        path:'signal', component:Signal
+    },  
+    {
+        path:'photos', component:Photos
+    },
+    {
+        path:'**', component:Notfound
+    }
+];
